c38d84639be10d2c60526479ab8f40824da81cb1
Katsuya Kikuchi <kikuchi.k.0711@gmail.com>
2016-07-13 15:13:56 +0900

    update file

--- test.cpp
+++ test.cpp
@@ -1,8 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-using namespace std;
+int main(int argc, char* argv[]) {
+    // ファイル入力ストリームの初期化
+    std::ifstream ifs("nazo.txt");
 
-int main(){
-	cout<<"test"<<endl;
-	return 0;
+    std::string line;
+    while (std::getline(ifs, line)) {
+        // ファイルの中身を一行づつ表示
+        std::cout << line << std::endl;
+    }
+
+    // ファイル出力ストリームの初期化
+    std::ofstream ofs("nazo.txt");
+
+    // ファイルに1行ずつ書き込み
+    ofs << "ham" << std::endl;
+    ofs << "eggkkaa" << std::endl;
+    ofs << "spam" << std::endl;
+
+    return 0;
 }
